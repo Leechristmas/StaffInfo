b55fd2cf50e3034092ac059d48db2c74e29af825
IKadzetau <ikadzetau@gomel.iba.by>
2016-10-12 11:25:53 +0300

    Interfaces for 'Unit Of Work' pattern (unit repository) and every entity repository were added.

---
+++ Staffinfo.DAL/Repositories/Interfaces/IUnitRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Staffinfo.DAL.Models;
+
+namespace Staffinfo.DAL.Repositories.Interfaces
+{
+    /// <summary>
+    /// Contract of 'Unit Of Work' pattern
+    /// </summary>
+    public interface IUnitRepository : IDisposable
+    {
+        IRepository<Address> AddressRepository { get; }
+        IRepository<Employee> EmployeeRepository { get; }
+        IRepository<Location> LocationRepository { get; }
+        IRepository<MesAchievement> MesAchievementRepository { get; }
+        IRepository<MilitaryService> MilitaryServiceRepository { get; }
+        IRepository<Passport> PassportRepository { get; }
+        IRepository<Post> PostRepository { get; }
+        IRepository<Rank> RankRepository { get; }
+        IRepository<Service> ServiceRepository { get; }
+        IRepository<WorkTerm> WorkTermRepository { get; }
+
+        void Save();
+    }
+}
---
+++ Staffinfo.DAL/Repositories/Interfaces/Irepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Staffinfo.DAL.Models.Common;
+
+namespace Staffinfo.DAL.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        /// <summary>
+        /// Returns all items
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> Select();
+
+        /// <summary>
+        /// Returns the item by id
+        /// </summary>
+        /// <param name="id">id of the item</param>
+        /// <returns></returns>
+        T Select(int id);
+
+        /// <summary>
+        /// Returns the item satisfying for the predicate
+        /// </summary>
+        /// <param name="predicate">search condition</param>
+        /// <returns></returns>
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
+
+        /// <summary>
+        /// Creates new item and saves this one in database
+        /// </summary>
+        /// <param name="item">item to save</param>
+        void Create(T item);
+
+        /// <summary>
+        /// Updates the item
+        /// </summary>
+        /// <param name="item">item to update</param>
+        void Update(T item);
+
+        /// <summary>
+        /// Removes the item from database
+        /// </summary>
+        /// <param name="id">id of the item to remove</param>
+        void Delete(int id);
+    }
+}
