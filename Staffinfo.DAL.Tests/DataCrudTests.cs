cd7c49885f882c9da113e19c0d7fb489bd8e937a
IKadzetau <ikadzetau@gomel.iba.by>
2016-10-12 14:42:21 +0300

    Unit Of Work patern was realized as Unit Repository

--- Staffinfo.DAL.Tests/DataCrudTests.cs
+++ Staffinfo.DAL.Tests/DataCrudTests.cs
@@ -1,31 +1,22 @@
 ﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Staffinfo.DAL.Context;
+using Staffinfo.DAL.Repositories;
 
 namespace Staffinfo.DAL.Tests
 {
+    /// <summary>
+    /// Includes unit-tests for Unit repository
+    /// </summary>
     [TestClass]
     public class DataCrudTests
     {
-        private readonly StaffContext _db = new StaffContext();
+        private StaffUnitRepository _staffUnitRepository = new StaffUnitRepository();
 
         [TestMethod]
-        public void Get_AllAddresses()
+        public void Select_GeyAllAddresses()
         {
-            var addr = _db.Addresses.ToList();
-
-            Assert.IsTrue(addr.Count > 0);
-        }
-
-        [TestMethod]
-        public void Get_AllPosts()
-        {
-            var t = _db.Posts.Find(1);
-            var posts = _db.Posts.ToList();
-
-            var t2 = posts[0].Service;
-
-            Assert.IsTrue(posts.Count > 0);
+            
         }
     }
 }
---
+++ Staffinfo.DAL/Repositories/StaffUnitRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Staffinfo.DAL.Context;
+using Staffinfo.DAL.Models;
+using Staffinfo.DAL.Repositories.Interfaces;
+
+namespace Staffinfo.DAL.Repositories
+{
+    /// <summary>
+    /// Unit repository (Unit Of Work pattern)
+    /// </summary>
+    public class StaffUnitRepository: IUnitRepository
+    {
+        private readonly StaffContext _staffContext;
+
+        public StaffUnitRepository()
+        {
+            _staffContext = new StaffContext();
+        }
+
+        private AddressRepository _addressRepository;
+        private EmployeeRepository _employeeRepository;
+        private LocationRepository _locationRepository;
+        private MesAchievementRepository _mesAchievementRepository;
+        private MilitaryServiceRepository _militaryServiceRepository;
+        private PassportRepository _passportRepository;
+        private PostRepository _postRepository;
+        private RankRepository _rankRepository;
+        private ServiceRepository _serviceRepository;
+        private WorkTermRepository _workTermRepository;
+
+        public IRepository<Address> AddressRepository => _addressRepository ?? new AddressRepository(_staffContext);
+        public IRepository<Employee> EmployeeRepository => _employeeRepository ?? new EmployeeRepository(_staffContext);
+        public IRepository<Location> LocationRepository => _locationRepository ?? new LocationRepository(_staffContext);
+
+        public IRepository<MesAchievement> MesAchievementRepository
+            => _mesAchievementRepository ?? new MesAchievementRepository(_staffContext);
+
+        public IRepository<MilitaryService> MilitaryServiceRepository
+            => _militaryServiceRepository ?? new MilitaryServiceRepository(_staffContext);
+
+        public IRepository<Passport> PassportRepository => _passportRepository ?? new PassportRepository(_staffContext);
+        public IRepository<Post> PostRepository => _postRepository ?? new PostRepository(_staffContext);
+        public IRepository<Rank> RankRepository => _rankRepository ?? new RankRepository(_staffContext);
+        public IRepository<Service> ServiceRepository => _serviceRepository ?? new ServiceRepository(_staffContext);
+        public IRepository<WorkTerm> WorkTermRepository => _workTermRepository ?? new WorkTermRepository(_staffContext);
+
+        public void Save()
+        {
+            _staffContext.SaveChanges();
+        }
+
+
+        #region IDisposable
+
+        private bool _disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    _staffContext.Dispose();
+                }
+                this._disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+    }
+}
