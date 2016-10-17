b54c6551ba9eeaff039315e70a5ab39bb4f2b5af
IKadzetau <ikadzetau@gomel.iba.by>
2016-10-17 15:28:06 +0300

    Staffinfo.Web project and suitable test project were added.

---
+++ Staffinfo.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Staffinfo.Web;
+using Staffinfo.Web.Controllers;
+
+namespace Staffinfo.Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
