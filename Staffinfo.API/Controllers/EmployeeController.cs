ac2eceb144eeff4a2dce526ed822e1b555121a05
IKadzetau <ikadzetau@gomel.iba.by>
2016-10-27 15:02:26 +0300

    API DI addition

--- Staffinfo.API/Controllers/EmployeeController.cs
+++ Staffinfo.API/Controllers/EmployeeController.cs
@@ -4,15 +4,24 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Staffinfo.DAL.Models;
+using Staffinfo.DAL.Repositories.Interfaces;
 
 namespace Staffinfo.API.Controllers
 {
     public class EmployeeController : ApiController
     {
+        private readonly IUnitRepository _repository;
+
+        public EmployeeController(IUnitRepository repository)
+        {
+            _repository = repository;
+        }
+
         // GET: api/Employee
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _repository.EmployeeRepository.Select().Select(e => e.EmployeeLastname);
         }
 
         // GET: api/Employee/5
