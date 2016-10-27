9b2c52db01ab26a87ca6044c2419013f394910ba
IKadzetau <ikadzetau@gomel.iba.by>
2016-10-27 14:04:52 +0300

    Revert "API refactoring"
    This reverts commit a6f6374f56664148b804640adb3fa7d0ef011c93.

--- Tests/Staffinfo.DAL.Tests/DatabaseTests.cs
+++ Tests/Staffinfo.DAL.Tests/DatabaseTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
 using Staffinfo.DAL.Context;
 using Staffinfo.DAL.Models;
 using Staffinfo.DAL.Repositories;
