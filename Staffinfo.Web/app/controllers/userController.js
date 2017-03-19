6075923fd875562a1fe865a5b743c324afd828b1
Dmitry Shevchuk <dshauchuk@gmail.com>
2017-03-19 13:35:12 +0300

    #users view

---
+++ Staffinfo.Web/app/controllers/userController.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+app.controller('userController',
+[
+    '$scope', function($scope) {
+
+        $scope.selected = null;
+
+        $scope.selectUser = function(user)
+        {
+            $scope.selected = user;
+        }
+
+        $scope.users = [
+            {
+                lastname: 'Иванов',
+                firstname: 'Иван',
+                middlename: 'Иванович'
+            },
+            {
+                lastname: 'Петров',
+                firstname: 'Петр',
+                middlename: 'Петрович'
+            },
+            {
+                lastname: 'Сидоров',
+                firstname: 'Алексей',
+                middlename: 'Сергеевич'
+            },
+            {
+                lastname: 'Александров',
+                firstname: 'Петр',
+                middlename: 'Геннадьевич'
+            }
+        ];
+
+
+
+    }
+]);
